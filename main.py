accd20840ea169887c27aa732b83575f9566226a
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-10-04 19:03:08 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,121 @@
+# ------------------------------------------------------------------
+# Sean Taylor Thomas
+#
+#
+#
+# -----------------------------------------------------------------
+
+def load_file(filename):
+    dataset = []
+    with open(filename) as f:
+        dataset = f.readlines()
+    return dataset
+
+
+def split_data(dataset):
+    spam = []
+    non_spam = []
+    for example in dataset:
+        if example[0:3] == "spa":
+            spam.append(example[5:-1].lower())
+        elif example[0:3] == "ham":
+            non_spam.append(example[4:-1].lower())
+        else:
+            print("No classification!")
+    return [spam, non_spam]
+
+
+def build_vocabulary(dataset):
+    """ Create a list of all unique words in class"""
+    vocab = []
+    for message in dataset:
+        words = message.split()
+        for word in words:
+            if word not in vocab:
+                vocab.append(word)
+    return vocab
+
+
+def build_frequency(dataset, vocab):
+    """ create list of frequencies for each unique word in class"""
+    freq = [0.0] * len(vocab)
+    for message in dataset:
+        words = message.split()
+        for word in words:
+                if word in vocab:
+                    freq[vocab.index(word)] += 1.0
+    # divide every element by total words of class
+    freq1 = []
+    for elem in freq:
+        freq1.append(elem/len(freq))
+    return freq1
+
+def sep_data(dataset):
+    test = []
+    train = []
+    for x in range(len(dataset)):
+        if x/len(dataset) <= .85:
+            train.append(dataset[x])
+        else:
+            test.append(dataset[x])
+    return [train, test]
+
+def naive_bayes(test, spam_dict, n_spam_dict, prob_spam, prob_n_spam):
+    probability_SPAM = prob_spam
+    probability_NOT_SPAM = prob_n_spam
+    test_words = test.split()
+    for word in test_words:
+        # calculating P(x | SPAM)
+        if spam_dict.get(word) is not None:
+            probability_SPAM *= spam_dict[word]
+        # calculating P(x | non-SPAM)
+        if n_spam_dict.get(word) is not None:
+            probability_NOT_SPAM *= n_spam_dict[word]
+    return [probability_SPAM, probability_NOT_SPAM]
+
+
+dataset = load_file('spam-data.txt')
+separate_data = sep_data(dataset)
+training_data = separate_data[0]
+test_data = separate_data[1]
+training_data = split_data(training_data)
+# create lists for spam and non-spam classes:
+spam = training_data[0]
+non_spam = training_data[1]
+
+# Calculate probabilities P(spam) and P(not_spam)
+prob_spam = len(spam) / (len(spam) + len(non_spam))
+prob_non_spam = len(non_spam) / (len(spam) / len(non_spam))
+
+# build vocabularies for each class, spam and not spam
+spam_vocab = build_vocabulary(spam)
+non_spam_vocab = build_vocabulary(non_spam)
+
+# build corresponding list to calculate relative frequency of each word in each class
+spam_freq = build_frequency(spam, spam_vocab)
+non_spam_freq = build_frequency(non_spam, non_spam_vocab)
+spam_dict = dict(zip(spam_vocab, spam_freq))
+non_spam_dict = dict(zip(non_spam_vocab, non_spam_freq))
+
+# calculate probabilities for testing input
+output = []
+split_test_data = split_data(test_data)
+
+# iterate through spam data, then non-spam data sets to test accuracy
+is_spam_set = 1
+for classes in split_test_data:
+    for single_test_data in classes:
+        output = naive_bayes(single_test_data, spam_dict, non_spam_dict, prob_spam, prob_non_spam)
+        probability_spam_test = output[0] * prob_non_spam
+        probability_not_spam_test = output[1] * prob_spam
+        if probability_spam_test > probability_not_spam_test and is_spam_set == 1:
+            print("We think it's spam, and we're correct")
+        elif probability_spam_test > probability_not_spam_test and is_spam_set != 1:
+            print("We think it's spam, but we're incorrect")
+        elif probability_spam_test < probability_not_spam_test and is_spam_set == 1:
+            print("We think it's not spam, but we're incorrect")
+        elif probability_spam_test < probability_not_spam_test and is_spam_set != 1:
+            print("We think it's not spam, and we're correct")
+        else:
+            print("They are equal")
+        is_spam_set -= 1
